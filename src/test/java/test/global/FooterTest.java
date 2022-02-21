a33a3e3b92ed50920d2cacaf31f0ae8fdc99d1dc
unknown <tqhoangcnttb94@gmail.com>
2022-02-22 00:07:11 +0700

    Lesson-22

--- src/test/java/test/global/FooterTest.java
+++ src/test/java/test/global/FooterTest.java
@@ -1,8 +1,10 @@
 package test.global;
 
+import driver.DriverFactory;
 import models.pages.CategoryPage;
 import models.pages.HomePage;
 import models.pages.RegisterPage;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import test.BaseTest;
 import test_flow.global.FooterTestFlow;
@@ -16,7 +18,9 @@ public class FooterTest extends BaseTest implements Urls {
 
     @Test
     public void testHomepageFooter() {
+        WebDriver driver = DriverFactory.getChromeDriver();
         driver.get(BASE_URL.concat(HOME_PAGE));
+
         try {
             FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
             footerTestFlow.verifyFooterComponent(HomePage.class);
@@ -29,6 +33,7 @@ public class FooterTest extends BaseTest implements Urls {
 
     @Test
     public void testRegisterPageFooter() {
+        WebDriver driver = DriverFactory.getChromeDriver();
         driver.get(BASE_URL.concat(REGISTER_PAGE));
         try {
             FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
--- src/test/java/test/global/TestNGAssertionType.java
+++ src/test/java/test/global/TestNGAssertionType.java
@@ -8,19 +8,20 @@ import org.testng.asserts.SoftAssert;
 public class TestNGAssertionType {
 
     @Test
-    public void testHardAssertion(){
-        Assert.assertEquals(1,2,"[ERR] Now 1 == 2");
-        try{
-            Assert.assertEquals(1,2,"[ERR] Now 1 == 2");
-        }catch(Exception ignored){}
-        System.out.println("Hello");
+    public void testHardAssertion() {
+        try {
+            Assert.assertEquals(1, 2, "[ERR] Now 1 == 2");
+        } catch (Exception ignored){
+
+            System.out.println("Hello!");
+        }
     }
 
     @Test
-    public void testSoftAssertion(){
+    public void testSoftAssertion() {
         SoftAssert softAssert = new SoftAssert();
-        Assert.assertEquals(1,2, "[ERR] Now 1 == 2");
-        Assert.assertEquals(1,21, "[ERR] Now 1 != 1");
-        softAssert.assertAll();
+        softAssert.assertEquals(1, 2, "[ERR] Now 1 == 2");
+        softAssert.assertEquals(1, 1, "[ERR] Now 1 != 1");
+//        softAssert.assertAll();
     }
 }
